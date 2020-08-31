e880dfa45ed986e295c5c3a69a7e9da828989825
DeeDowns <dee_downs@outlook.com>
2020-08-31 19:52:50 -0400

    working on delete and put requests

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ let users = [
     }
 ]
 
+//GET all users
 server.get('/api/users', (req, res) => {
-    res.status(200).json({ data: users})
+    users ? res.status(200).json({ data: users}) : res.status(500).json({ errorMessage: "The users information could not be retrieved." })
 })
 
+//GET user by id
 server.get('/api/users/:id', (req, res) => {
     const id = (req.params.id) 
 
@@ -25,16 +27,67 @@ server.get('/api/users/:id', (req, res) => {
     if(found) {
         res.status(200).json(found)
     } else {
-        res.status(400).json({ message: 'not found'})
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    }
+
+    try {
+        res.status(200).json({ data: found })
+    } catch (error) {
+        res.status(500).json({ errorMessage: "The user information could not be retrieved." })
     }
 })
 
+//POST new user
 server.post('/api/users', (req, res) => {
     const user = req.body
     user.id = shortid.generate()
     users.push(user)
-    user.name && user.bio ? res.status(201).json({ data: user }) : res.status(400).json({ message: 'provide name and bio'})
+    user.name && user.bio ? res.status(201).json({ data: user }) : res.status(400).json({ Errormessage: 'Please provide name and bio for the user.'})
+    try {
+        res.status(201).json({ data: user })
+    } catch (error) {
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
+    }
+
 })
+
+//DELETE user by id
+server.delete('/api/users/:id'), (req, res) => {
+    const id = (req.params.id)
+    users = users.filter(user => user.id !== id)
+
+    if(!user.id) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    }
+
+    try {
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json({ message:  "The user could not be removed" })
+    }
+ 
+}
+
+//PUT request
+server.put('api/users/:id'), (req, res) => {
+    const changes = req.body
+    const id = (req.body)
+
+    let found = users.find(user => user.id === id)
+
+
+    if(found) {
+        Object.assign(found, changes)
+        res.status(200).json(found)
+    } else {
+        res.status(404).json({ message: 'The user with the specified ID does not exist'})
+    }
+
+    users = users.filter(user => user.id !== id)
+
+    res.status(200).json(users)
+
+}
 
 const port = 8000
 server.listen(port, () => {
