f9a239e6bc77ca5342eb2ff83a6d473d8d6b7c28
DeeDowns <dee_downs@outlook.com>
2020-08-31 17:11:22 -0400

    completed POST request and GET user by id request

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ server.get('/api/users', (req, res) => {
     res.status(200).json({ data: users})
 })
 
+server.get('/api/users/:id', (req, res) => {
+    const id = (req.params.id) 
+
+    let found = users.find(user => user.id === id)
+
+    if(found) {
+        res.status(200).json(found)
+    } else {
+        res.status(400).json({ message: 'not found'})
+    }
+})
+
+server.post('/api/users', (req, res) => {
+    const user = req.body
+    user.id = shortid.generate()
+    users.push(user)
+    user.name && user.bio ? res.status(201).json({ data: user }) : res.status(400).json({ message: 'provide name and bio'})
+})
+
 const port = 8000
 server.listen(port, () => {
     console.log(`listening on server ${port}`)
